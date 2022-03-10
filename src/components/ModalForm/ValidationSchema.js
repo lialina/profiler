856649c1fbe278a57cb6171c679ff0d4ebd575b4
Anastasiia Lialina <lialina.anastasiia@gmail.com>
2022-03-10 17:35:28 +0100

    Phone validation has been added

--- src/components/ModalForm/ValidationSchema.js
+++ src/components/ModalForm/ValidationSchema.js
@@ -1,5 +1,8 @@
 import * as Yup from "yup";
 
+const phoneRegExp =
+  /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
+
 const validationSchema = Yup.object({
   firstName: Yup.string()
     .max(15, "Must be 15 characters or less")
@@ -7,7 +10,8 @@ const validationSchema = Yup.object({
   lastName: Yup.string()
     .max(20, "Must be 20 characters or less")
     .required("Required"),
-  email: Yup.string().email("Invalid email adress").required("Required"),
+  phone: Yup.string().matches(phoneRegExp, "Phone number is not valid"),
+  email: Yup.string().email("Email adress is not valid").required("Required"),
   bio: Yup.string().max(200, "Must be 200 characters or less"),
 });
 
