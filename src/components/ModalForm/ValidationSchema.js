cd98f3cacd5b0f403bef9db02f1dde931ef2860c
Anastasiia Lialina <lialina.anastasiia@gmail.com>
2022-03-22 03:08:24 +0100

    Comments on UI ValidationSchema have been removed

--- src/components/ModalForm/ValidationSchema.js
+++ src/components/ModalForm/ValidationSchema.js
@@ -3,19 +3,20 @@ import "yup-phone";
 
 const validationSchema = Yup.object({
   firstName: Yup.string()
-    // .max(15, "Must be 15 characters or less")
+    .max(15, "First name must contain 15 characters or less")
     .required("Required"),
   lastName: Yup.string()
-    // .max(20, "Must be 20 characters or less")
+    .max(20, "Last name must contain 20 characters or less")
     .required("Required"),
   phone: Yup.string()
-    // .phone(null, true, "Phone number is not valid")
+    .phone(null, true, "Phone must start with + and contain only numbers")
     .required("Required"),
   email: Yup.string()
-    // .email("Email adress is not valid")
+    .email(
+      "Email must include the username, an @ symbol, domain name, a dot, and the domain"
+    )
     .required("Required"),
-  bio: Yup.string(),
-  // .max(200, "Must be 200 characters or less"),
+  bio: Yup.string().max(200, "Bio must contain no more than 200 characters"),
 });
 
 export default validationSchema;
