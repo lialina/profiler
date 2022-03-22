62521c2c83c85f4a81d1fd0cfbcd6d384870aa4d
Anastasiia Lialina <lialina.anastasiia@gmail.com>
2022-03-22 03:04:22 +0100

    Server validation has been updated

--- server.js
+++ server.js
@@ -32,53 +32,42 @@ app
 
   .post("/profiles", async (req, res, next) => {
     try {
-      try {
-        const errorMessage = new Object();
+      const errors = {};
 
-        if (!(req.body.firstName.length <= 15)) {
-          errorMessage.firstName =
-            "First name must contain 15 characters or less";
-        }
-
-        if (!(req.body.lastName.length <= 20)) {
-          errorMessage.lastName =
-            "Last name must contain 15 characters or less";
-        }
-
-        if (!/^\+(?:[0-9] ?){6,14}[0-9]$/.test(req.body.phone)) {
-          errorMessage.phone =
-            "Phone must start with + and contain only numbers";
-        }
-
-        if (
-          !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(req.body.email)
-        ) {
-          errorMessage.email =
-            "Email must include the username, an @ symbol, domain name, a dot, and the domain";
-        }
-
-        if (!(req.body.bio.length <= 200)) {
-          errorMessage.bio = "Bio must contain no more than 200 characters";
-        }
-
-        if (Object.keys(errorMessage).length > 0) {
-          return res
-            .status(400)
-            .json({ status: "fail", code: 400, error: { errorMessage } });
-        }
-      } catch (error) {
-        next(error);
+      if (req.body.firstName.length > 15) {
+        errors.firstName = "First name must contain 15 characters or less";
       }
 
-      const newProfile = {
+      if (req.body.lastName.length > 20) {
+        errors.lastName = "Last name must contain 20 characters or less";
+      }
+
+      if (!/^\+(?:[0-9] ?){6,14}[0-9]$/.test(req.body.phone)) {
+        errors.phone = "Phone must start with + and contain only numbers";
+      }
+
+      if (
+        !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(req.body.email)
+      ) {
+        errors.email =
+          "Email must include the username, an @ symbol, domain name, a dot, and the domain";
+      }
+
+      if (req.body.bio.length > 200) {
+        errors.bio = "Bio must contain no more than 200 characters";
+      }
+
+      if (Object.keys(errors).length > 0) {
+        return res.status(400).json({ status: "fail", code: 400, errors });
+      }
+
+      const data = {
         id: crypto.randomUUID(),
         ...req.body,
       };
-      profiles.push(newProfile);
+      profiles.push(data);
 
-      res
-        .status(201)
-        .json({ status: "success", code: 201, data: { newProfile } });
+      res.status(201).json({ status: "success", code: 201, data });
     } catch (error) {
       next(error);
     }
