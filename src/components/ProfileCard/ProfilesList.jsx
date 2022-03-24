08ecf9be6ca0c9f304aa5495faf1c115a998ca7d
Anastasiia Lialina <lialina.anastasiia@gmail.com>
2022-03-24 19:22:47 +0100

    Selector name changed and conditional rendering added in the ProfilesList

--- src/components/ProfileCard/ProfilesList.jsx
+++ src/components/ProfileCard/ProfilesList.jsx
@@ -3,20 +3,20 @@ import { useSelector } from "react-redux";
 import ProfileCard from "./ProfileCard";
 
 const ProfilesList = () => {
-  const state = useSelector((state) => state.profiles);
-  console.log("state", state);
+  const profiles = useSelector((state) => state.profiles);
   return (
     <ul>
-      {state.map(({ id, firstName, lastName, phone, email, bio }) => (
-        <ProfileCard
-          key={id}
-          firstName={firstName}
-          lastName={lastName}
-          phone={phone}
-          email={email}
-          bio={bio}
-        />
-      ))}
+      {profiles.length > 0 &&
+        profiles.map(({ id, firstName, lastName, phone, email, bio }) => (
+          <ProfileCard
+            key={id}
+            firstName={firstName}
+            lastName={lastName}
+            phone={phone}
+            email={email}
+            bio={bio}
+          />
+        ))}
     </ul>
   );
 };
