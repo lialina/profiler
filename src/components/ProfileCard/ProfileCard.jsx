ccec4bd56f358e66c64e7d14bd68b19b138c90dd
Anastasiia Lialina <lialina.anastasiia@gmail.com>
2022-03-25 16:42:54 +0100

    Initial card styles have been added

--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-
+import { StyledProfileItem } from "./ProfileStyles";
 const ProfilesCard = ({ id, firstName, lastName, phone, email, bio }) => {
   return (
-    <li key={id}>
+    <StyledProfileItem key={id}>
       <p>{firstName}</p>
       <p>{lastName}</p>
       <p>{email}</p>
       <p>{phone}</p>
       <p>{bio}</p>
-    </li>
+    </StyledProfileItem>
   );
 };
 
---
+++ src/components/ProfileCard/ProfileStyles.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+const StyledProfileItem = styled.li`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  width: 100%;
+  max-width: 300px;
+  margin: auto;
+
+  border-radius: 6px;
+  // margin-bottom: 50px;
+
+  background-color: white;
+  box-shadow: 0 0 6px rgba(0, 0, 0, 0.2);
+`;
+
+// const  = styled.p`
+
+// `;
+
+export { StyledProfileItem };
