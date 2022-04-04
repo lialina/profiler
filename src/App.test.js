32be0adc5f102cb74a514f4c9df9f4f5274d24ad
Anastasiia Lialina <lialina.anastasiia@gmail.com>
2022-04-04 10:35:09 +0200

    App unit tests have been updated

--- src/App.test.js
+++ src/App.test.js
@@ -1,39 +1,30 @@
 import React from "react";
 import * as redux from "react-redux";
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
 import App from "./App";
+import Modal from "./components/Modal/Modal";
+import ProfilesList from "./components/ProfileCard/ProfilesList";
 
 describe("App component", () => {
   const useDispatchMock = jest.spyOn(redux, "useDispatch");
   useDispatchMock.mockReturnValue(jest.fn());
 
   const useSelectorMock = jest.spyOn(redux, "useSelector");
-  // ...Does nor matter in this component, why?
-  // ...Because of 2 possible options in conditional rendering in terms of isModalVisible value?
-  useSelectorMock.mockReturnValue({ isModalVisible: true });
 
   it("renders without crashing", () => {
-    shallow(<App />);
-  });
-
-  // OR
-  it("renders without crashing 2", () => {
     const wrapper = shallow(<App />);
     expect(wrapper.exists()).toBeTruthy();
   });
 
   it("renders Modal component in it if isModalVisible is true without crashing", () => {
-    // ...The same, isModalVisible value does not matter, why?
-    useSelectorMock.mockReturnValue({ isModalVisible: true });
-    const wrapper = shallow(<App />);
-    expect(wrapper.find("Modal")).toHaveLength(1);
+    useSelectorMock.mockReturnValue(true);
+    const wrapper = mount(<App />);
+    expect(wrapper.exists(Modal)).toBe(true);
   });
 
-  // ...Fails because of empty profiles in ProfilesList?
-  // it("renders ProfilesList component in it if isModalVisible is false without crashing", () => {
-  //   useSelectorMock.mockReturnValue({ isModalVisible: false });
-  //   const wrapper = shallow(<App />);
-  //   expect(wrapper.find("ProfilesList")).toHaveLength(1);
-  //   // expect(wrapper.exists()).toBeTruthy();
-  // });
+  it("renders ProfilesList component in it if isModalVisible is false without crashing", () => {
+    useSelectorMock.mockReturnValue(false);
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(ProfilesList)).toHaveLength(1);
+  });
 });
