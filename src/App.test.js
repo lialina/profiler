f79d61416201ec16f8dcc1e247b846b22ad57a6e
Anastasiia Lialina <lialina.anastasiia@gmail.com>
2022-04-09 00:36:59 +0200

    App unit tests have been updated

--- src/App.test.js
+++ src/App.test.js
@@ -3,19 +3,13 @@ import * as redux from "react-redux";
 import { shallow, mount } from "enzyme";
 import App from "./App";
 import Modal from "./components/Modal/Modal";
+import Button from "../src/components/Button/Button";
 import ProfilesList from "./components/ProfileCard/ProfilesList";
 import { toggleModal } from "./redux/modalSlice";
-// import { store } from "../src/redux/store";
 import { ModalButton } from "../src/components/Modal/ModalStyles";
-import configureStore from "redux-mock-store";
-
-const mockStore = configureStore();
-const store = mockStore();
 
 describe("App component", () => {
   const useDispatchMock = jest.spyOn(redux, "useDispatch");
-  useDispatchMock.mockReturnValue(jest.fn());
-
   const useSelectorMock = jest.spyOn(redux, "useSelector");
 
   it("renders without crashing", () => {
@@ -35,45 +29,27 @@ describe("App component", () => {
     expect(wrapper.find(ProfilesList)).toHaveLength(1);
   });
 
-  it("dispatch", async () => {
-    const expectedActions = [
-      {
-        payload: undefined,
-        type: "modal/toggleModal",
-      },
-    ];
-    store.dispatch(toggleModal());
-    const actions = store.getActions();
-    // console.log(actions);
-    expect(actions).toEqual(expectedActions);
-
-    // const expectedActions = {
-    //   payload: undefined,
-    //   type: "modal/toggleModal",
-    // };
-    // expect(store.dispatch(toggleModal())).toEqual(expectedActions);
-  });
-
-  it("closeModal", () => {
-    // const expectedActions = {
-    //   payload: undefined,
-    //   type: "modal/toggleModal",
-    // };
-
+  it("openModal with onClick on Button in Header", () => {
+    const mockedDispatch = jest.fn();
+    useDispatchMock.mockReturnValue(mockedDispatch);
     useSelectorMock.mockReturnValue(true);
 
-    const closeModal = jest.fn();
-    const wrapper = mount(<App onClick={closeModal()} />);
-    const useDispatchMock = jest.spyOn(redux, "useDispatch");
-    useDispatchMock.mockReturnValue(jest.fn());
+    const wrapper = mount(<App />);
+    const button = wrapper.find(Button);
+    button.simulate("click");
+
+    expect(mockedDispatch).toHaveBeenCalledWith(toggleModal());
+  });
+
+  it("closeModal with onClick on ModalButton", () => {
+    const mockedDispatch = jest.fn();
+    useDispatchMock.mockReturnValue(mockedDispatch);
+    useSelectorMock.mockReturnValue(true);
+
+    const wrapper = mount(<App />);
     const button = wrapper.find(ModalButton);
     button.simulate("click");
-    // console.log(button.debug());
 
-    // store.dispatch(toggleModal());
-    // const actions = store.getActions();
-    // console.log(actions);
-    // expect(actions).toEqual(expectedActions);
-    expect(closeModal).toHaveBeenCalledTimes(1);
+    expect(mockedDispatch).toHaveBeenCalledWith(toggleModal());
   });
 });
