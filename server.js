8fca2a9dc0d794943cb7fe34312bf3183f5afb4c
Anastasiia Lialina <lialina.anastasiia@gmail.com>
2022-04-26 14:37:09 +0200

    Validation in put http request on server has been added

--- server.js
+++ server.js
@@ -93,6 +93,35 @@ app
 
   .put("/profiles/:id", async (req, res, next) => {
     try {
+      const errors = {};
+
+      if (req.body.firstName.length > 15) {
+        errors.firstName = "First name must contain 15 characters or less";
+      }
+
+      if (req.body.lastName.length > 20) {
+        errors.lastName = "Last name must contain 20 characters or less";
+      }
+
+      if (!/^\+(?:[0-9] ?){6,14}[0-9]$/.test(req.body.phone)) {
+        errors.phone = "Phone must start with + and contain only numbers";
+      }
+
+      if (
+        !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(req.body.email)
+      ) {
+        errors.email =
+          "Email must include the username, an @ symbol, domain name, a dot, and the domain";
+      }
+
+      if (req.body.bio.length > 200) {
+        errors.bio = "Bio must contain no more than 200 characters";
+      }
+
+      if (Object.keys(errors).length > 0) {
+        return res.status(400).json({ status: "fail", code: 400, errors });
+      }
+
       profiles = profiles.map((profile) =>
         profile.id === req.params.id ? req.body : profile
       );
